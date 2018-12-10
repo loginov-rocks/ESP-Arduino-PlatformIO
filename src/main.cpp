6d069d9dbc42f97ff04e104087e6d706998049b5
Danila Loginov <danila@loginov.rocks>
2018-12-11 01:00:54 +0300

    blink the world

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <Arduino.h>
 
+#define LED D0
+
 void setup()
 {
-  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED, LOW);
+  delay(500);
+  digitalWrite(LED, HIGH);
+  delay(1000);
 }
